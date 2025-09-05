d6e43eee56aceec11b510e0a42868651f5495548
Nikolay Milenko <nikolay.milenko@gmail.com>
2025-09-05 14:52:09 +0200

    init spring project

---
+++ backend/src/test/java/com/massivemarketmanager/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.massivemarketmanager.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
