320fe176a2c2caecae36cc291eab33ae75a0efd2
Nikolay Milenko <nikolay.milenko@gmail.com>
2025-09-30 22:26:48 +0200

    refactor: disable couple of fields for now

--- backend/src/main/java/com/massivemarketmanager/backend/auth/SignInRequestDto.java
+++ backend/src/main/java/com/massivemarketmanager/backend/auth/SignInRequestDto.java
@@ -9,7 +9,7 @@ import java.util.Map;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record SignInRequestDto(
         @NotBlank @Email @Size(max = 254) String email,
-        @NotBlank @Size(min = 8, max = 72) String password,
-        @NotNull UUID deviceId,
-        @Size(max = 32) Map<@NotBlank String, @Size(max = 128) String> metadata
+        @NotBlank @Size(min = 8, max = 72) String password
+        // @NotNull UUID deviceId,
+        // @Size(max = 32) Map<@NotBlank String, @Size(max = 128) String> metadata
 ) {}
