ca3c9854dc341a1713e42b00632c88b0976616c7
Artyom Zagorskiy <artyomzagorskiy@gmail.com>
2025-10-11 12:21:27 +0200

    Created Settings Template in Dashboard
    Added settings route to the dashboard and created a basic settings page for future customization.

--- frontend/src/components/dashboard/base/app-sidebar.jsx
+++ frontend/src/components/dashboard/base/app-sidebar.jsx
@@ -31,7 +31,7 @@ const data = {
   navSecondary: [
     {
       title: "Settings",
-      url: "#",
+      url: "/dashboard/settings",
       icon: IconSettings,
     }
   ],
--- frontend/src/components/dashboard/base/nav-secondary.jsx
+++ frontend/src/components/dashboard/base/nav-secondary.jsx
@@ -7,6 +7,7 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar"
+import { NavLink } from "react-router-dom"
 
 export function NavSecondary({ items, ...props }) {
   return (
@@ -15,12 +16,20 @@ export function NavSecondary({ items, ...props }) {
         <SidebarMenu>
           {items.map((item) => (
             <SidebarMenuItem key={item.title}>
-              <SidebarMenuButton asChild>
-                <a href={item.url}>
-                  <item.icon className="size-4" />
-                  <span>{item.title}</span>
-                </a>
-              </SidebarMenuButton>
+              <NavLink to={item.url} className="w-full">
+                {({ isActive }) => (
+                  <SidebarMenuButton
+                    tooltip={item.title}
+                    asChild
+                    className={isActive ? "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground" : "hover:bg-muted hover:text-foreground"}
+                  >
+                    <div className="flex items-center gap-2">
+                      {item.icon && <item.icon className="w-5 h-5" />}
+                      <span>{item.title}</span>
+                    </div>
+                  </SidebarMenuButton>
+                )}
+              </NavLink>
             </SidebarMenuItem>
           ))}
         </SidebarMenu>
